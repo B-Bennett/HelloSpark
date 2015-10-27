f7fa0b471237d1414dd7eb519b51f63892337d5b
BennettIronYard <bennettiy12@gmail.com>
2015-10-27 11:26:04 -0400

    add sessions

--- src/com/Instio/Main.java
+++ src/com/Instio/Main.java
@@ -2,6 +2,7 @@ package com.Instio;
 
 import com.github.mustachejava.Mustache;
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -11,29 +12,51 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<User> users = new ArrayList();
-        Spark.staticFileLocation("public");
+        HashMap<String, User> users = new HashMap();
         Spark.init();
         Spark.post(
                 "/create-account",
                 ((request, response) -> {
-                    User user = new User();
-                    user.name = request.queryParams("username"); //Requesting the username
-                    user.password = request.queryParams("password");
-                    users.add(user);
-                    response.redirect("/"); // "/" represents the top level. return homepage
+                    String name = request.queryParams("username");
+                    String password = request.queryParams("password");
+
+                    Session session = request.session();
+                    session.attribute("username", name);
+
+                    if (users.get(name) == null) {
+                        User user = new User();
+                        user.name = name;
+                        user.password = password;
+                        users.put(name, user);
+                    }
+                    response.redirect("/");
                     return "";
                 })
         );
-        Spark.get( //just returning info
-                "/accounts",
+        Spark.get(
+                "/",
                 ((request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("username");
+                    if (name != null) {
+                        HashMap m = new HashMap();
+                        m.put("count", users.size());
+                        m.put("accounts", users.values());
+                        return new ModelAndView(m, "logged-in.html");
+                    }
                     HashMap m = new HashMap();
-                    m.put("count", users.size());
-                    m.put("accounts", users);
-                    return new ModelAndView(m, "accounts.html");
+                    return new ModelAndView(m, "not-logged-in.html");
                 }),
                 new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/logout",
+                ((request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
+                })
         );
     }
 }
